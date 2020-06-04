b7952f0d4907eba1895996aac703fd1019023917
Rajan15 <rajan.forever.90@gmail.com>
2020-06-04 12:37:13 +0530

    my first commit

---
+++ src/test/java/com/rajan/mavenDemo/AppiumTest.java
@@ -0,0 +1,20 @@
+package com.rajan.mavenDemo;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void appiumLogin() {
+		System.out.println("appiumLogin");
+	}
+	
+	
+	@Test
+	public void appiumLogout() {
+		System.out.println("appiumLogout");
+	}
+	
+	
+}
---
+++ src/test/java/com/rajan/mavenDemo/SeleniumDemo.java
@@ -0,0 +1,17 @@
+package com.rajan.mavenDemo;
+
+import org.testng.annotations.Test;
+
+public class SeleniumDemo {
+
+	@Test
+	public void seleniumLogin() {
+		System.out.println("seleniumLogin");
+	}
+	
+	@Test
+	public void seleniumLogout() {
+		System.out.println("seleniumLogout");
+	}
+}
+
